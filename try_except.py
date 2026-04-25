30ec9363c45d50cc1a53f6522a30d62b0eb70b42
saibhargava2552008 <s.saibhargav2552008@gmail.com>
2026-04-25 15:17:08 +0530

    Add files via upload

---
+++ try_except.py
@@ -0,0 +1,6 @@
+try:
+    marks = int(input("Enter your marks: "))
+    result = marks / 0
+    print("Your result is:", result)
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")    
